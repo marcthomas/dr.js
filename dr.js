52bdd3183eb753c70404aaa6242bd3e7ade617d2
Denis Ciccale <dciccale@gmail.com>
2012-02-04 18:47:54 +0100

    code a little cleaner + fix 2 issues on Windows (1)using the json file (2)copying the css file

--- dr.js
+++ dr.js
@@ -1,5 +1,5 @@
 /*
- * Dr.js 0.0.5 - Simple JavaScript Documentation
+ * Dr.js 0.0.6 - Simple JavaScript Documentation
  *
  * Copyright (c) 2011 Dmitry Baranovskiy (http://dmitry.baranovskiy.com/)
  * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
@@ -10,36 +10,36 @@ var fs = require("fs"),
     format = require("./formatter.js"),
     _ref = require("child_process"),
     spawn = _ref.spawn,
-    exec = _ref.exec;
+    exec = _ref.exec,
+    isWindows = process.platform === "win32",
+    slash = isWindows ? "\\" : "/",
+    docsDir = "docs";
 
 function getPath(filepath) {
-    return "docs/" + path.basename(filepath, path.extname(filepath));
+    return docsDir + slash + path.basename(filepath, path.extname(filepath));
 }
 
 function main(files, targetDir, reporter) {
+    var mkdir = isWindows ? "mkdir " : "mkdir -p ",
+        cp = isWindows ? "copy " : "cp ",
+        dirname = isWindows ? "\"" + __dirname + "\"" : __dirname;
+
     // default target dir to the cwd
     targetDir = targetDir || process.cwd();
-    
+
     // default the reporter to the console
     reporter = reporter || console.log;
-    
-    // if windows, change commands and slashes
-    if(!!process.platform.match(/^win/)) {
-        exec("mkdir " + path.resolve(targetDir, "docs"));
-        exec("copy " + __dirname + "\\dr.css " + path.resolve(targetDir, "docs\\dr.css"));
-    }
-    else {
-        exec("mkdir -p " + path.resolve(targetDir, "docs"));
-        exec("cp " + __dirname + "/dr.css " + path.resolve(targetDir, "docs/dr.css"));
-    }
 
     var srcs = [],
         chunks = {},
         title = "",
         output = "",
+        outputDir = "",
         scripts = [],
         fileName,
-        toc = [];
+        toc = [],
+        i, l;
+
     files.splice(0, 2);
 
     if (!files.length) {
@@ -47,11 +47,11 @@ function main(files, targetDir, reporter) {
         process.exit(1);
     }
 
-    if (files.length == 1 && path.extname(files[0]) == ".json") {
+    if (files.length === 1 && path.extname(files[0]) === ".json") {
         var json = JSON.parse(fs.readFileSync(files[0], "utf-8"));
         title = json.title;
         files = [];
-        for (var i = 0, ii = json.files.length; i < ii; i++) {
+        for (i = 0, l = json.files.length; i < l; i++) {
             files.push(json.files[i].url);
             srcs.push(json.files[i].link);
         }
@@ -59,8 +59,12 @@ function main(files, targetDir, reporter) {
         scripts = json.scripts || [];
     }
 
+    // create docs dir
+    outputDir = output.split("/")[0] || docsDir;
+    exec(mkdir + path.resolve(targetDir, outputDir));
+
     reporter("Trust me, I am a Dr.js");
-    for (i = 0, ii = files.length; i < ii; i++) {
+    for (i = 0, l = files.length; i < l; i++) {
         var filename = files[i];
         fileName = fileName || filename;
         reporter("Processing " + filename);
@@ -84,6 +88,7 @@ function main(files, targetDir, reporter) {
             break;
         }
     }
+
     var TOC = "",
         RES = "";
     toc.sort(function (a, b) {
@@ -95,21 +100,28 @@ function main(files, targetDir, reporter) {
         }
         return 1;
     });
-    for (i = 0, ii = toc.length; i < ii; i++) if (!i || toc[i].name != toc[i - 1].name) {
+    for (i = 0, l = toc.length; i < l; i++) if (!i || toc[i].name != toc[i - 1].name) {
         TOC += format('<li class="dr-lvl{indent}"><a href="#{name}" class="{clas}"><span>{name}{brackets}</span></a></li>', toc[i]);
         RES += chunks[toc[i].name] || "";
     }
     var html = '<!DOCTYPE html>\n<!-- Generated with Dr.js -->\n<html lang="en"><head><meta charset="utf-8"><title>' + title + ' Reference</title><link rel="stylesheet" href="dr.css" media="screen"><link rel="stylesheet" href="dr-print.css" media="print"></head><body id="dr-js"><div id="dr"><ol class="dr-toc" id="dr-toc">' + TOC + '</ol><div class="dr-doc"><h1>' + title + ' Reference</h1>' + RES + "</div></div>\n";
-    for (i = 0, ii = scripts.length; i < ii; i++) {
+    for (i = 0, l = scripts.length; i < l; i++) {
         html += '<script src="' + scripts[i] + '"></script>\n';
     }
     html += "</body></html>";
-    fs.writeFile(output || (getPath(fileName) + ".html"), html, function () {
+
+    fs.writeFile(output || (getPath(fileName) + ".html"), html, function (error) {
+        if(error) {
+            throw error;
+        }
         reporter("Saved to \033[32m" + (output || getPath(fileName) + ".html") + "\033[0m");
     });
+
+    // copy css file into the output directory
+    exec(cp + dirname + slash + "dr.css " + path.resolve(targetDir, outputDir + slash + "dr.css"));
 }
 
-// if dr.js has been required, then return the main function 
+// if dr.js has been required, then return the main function
 if (module.parent) {
     module.exports = main;
 }
